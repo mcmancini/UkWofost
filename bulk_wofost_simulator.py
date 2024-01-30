46e6638e6884f67a70e9e97f2952afede0454b67
mcmancini <m.c.mancini@exeter.ac.uk>
2024-01-30 13:03:15 +0000

    Added script to run WOFOST in bulk for various input parameters specified in a csv input file. Each row in the input specifis a crop with its parameter set; multiple rows can be aggregated to build rotations

--- bulk_wofost_simulator.py
+++ bulk_wofost_simulator.py
@@ -23,22 +23,102 @@ which correspond to the same rotation identifier) using
 instances of the CropRotation class
 """
 
+import argparse
 import pandas as pd
-from ukwofost.crop_manager import Crop, CropBuilder
+from ukwofost.crop_manager import Crop, CropBuilder, CropRotation
+from ukwofost.defaults import wofost_parameters
 from ukwofost.simulation_manager import WofostSimulator
 from ukwofost.utils import lonlat2osgrid
 
-# Load csv sample file
-sample_runs = pd.read_csv("resources/Wofost_test.csv")
 
-sample = sample_runs.iloc[0, :]
+# pylint: disable=R0914
+def run_rotations(input_sample_df, output_filename):
+    """
+    Function that reads the rows of a csv file containing
+    input parameters for runs of Wofost, builds crop rotations
+    which are then run through Wofost returning a dataframe with
+    all WOFOST output parameters
+    """
+    lonlat = input_sample_df[["lon", "lat"]]
+    unique_pairs = lonlat.drop_duplicates(subset=["lon", "lat"])
+    crop_yield = pd.DataFrame()
+    # Iterate over unique locations
+    for _, row in unique_pairs.iterrows():
+        lon, lat = row["lon"], row["lat"]
+        # initialise simulator
+        os_code = lonlat2osgrid((lon, lat), 10)
+        sim = WofostSimulator(os_code)
+        lonlat_df = input_sample_df[
+            (input_sample_df["lon"] == lon) & (input_sample_df["lat"] == lat)
+        ]
 
-crop_args = CropBuilder(sample)
-crop_params = crop_args.crop_parameters
-crop = Crop(crop_args.calendar_year, crop_args.crop, **crop_params)
+        # Iterate over rotations
+        for rotation in lonlat_df["name"].unique():
+            rotation_df = lonlat_df[lonlat_df["name"] == rotation]
+            for item in rotation_df["iteration"].unique():
+                item_df = rotation_df[rotation_df["iteration"] == item].sort_values(
+                    by="year"
+                )
 
-LON, LAT = -3.4111140552800747, 57.13317708272391
-os_code = lonlat2osgrid((LON, LAT), 10)
-sim = WofostSimulator(os_code)
+                print(
+                    f"Running simulator for '{rotation}' and iteration "
+                    f"'{str(item)}' in location '{os_code}'"
+                )
+                # Deal with parameters overridden by user (e.g., initial soil
+                # conditions, or Wofost crop parameters)
+                parameter_dict = item_df.iloc[0, :].to_dict()
+                nonstandard_parameters = {
+                    key: value
+                    for key, value in parameter_dict.items()
+                    if key in wofost_parameters
+                }
 
-crop_yield = sim.run(crop)
+                # Build rotation
+                crops_in_rotation = []
+                for _, row in item_df.iterrows():
+                    # Agromanagement of crop rotation
+                    crop_args = CropBuilder(row)
+                    crop_params = crop_args.crop_parameters
+                    crop = Crop(crop_args.calendar_year, crop_args.crop, **crop_params)
+                    crops_in_rotation.append(crop)
+
+                crop_rotation = CropRotation(crops_in_rotation)
+                rotation_output = sim.run(
+                    crop_rotation, **nonstandard_parameters
+                ).reset_index(drop=False)
+                rotation_output["lon"], rotation_output["lat"] = lon, lat
+                rotation_output["rotation"] = rotation
+                rotation_output["iteration"] = item
+                crop_yield = pd.concat([crop_yield, rotation_output], ignore_index=True)
+                print("Done...")
+    crop_yield.to_csv(output_filename, index=False)
+    print("All jobs completed\n------------------")
+
+
+# pylint: disable=R0914
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the Wofost Crop yield simulator")
+    parser.add_argument(
+        "-i", "--input", type=str, required=True, help="Path to the input CSV file"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        required=True,
+        help="Path of the folder where the output CSV files will be saved",
+    )
+
+    args = parser.parse_args()
+    input_file = args.input
+    output_file = args.output
+    try:
+        input_df = pd.read_csv(input_file)
+        run_rotations(input_df, output_file)
+    except FileNotFoundError:
+        print(f"File not found: {input_file}")
+    # pylint: disable=W0718
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    # pylint: disable=W0718
