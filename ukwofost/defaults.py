d301fc62d1ad706dafeb443bf85faa393e427540
mcmancini <m.c.mancini@exeter.ac.uk>
2024-02-08 17:00:12 +0000

    Added standard moisture at harvest for all crops in defaults.py

--- ukwofost/defaults.py
+++ ukwofost/defaults.py
@@ -187,3 +187,12 @@ wofost_parameters = set(
         "WAV",
     ]
 )
+
+moisture_adjustment = {
+    "wheat": 0.145,
+    "barley": 0.145,
+    "rapeseed": 0.9,
+    "potato": 0.79,
+    "rye_grass": 0.75,
+    "fallow": None,
+}
