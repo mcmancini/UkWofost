4e971ea32b8f456e65883432c5d6b8cba98db608
mcmancini <m.c.mancini@exeter.ac.uk>
2025-08-04 17:04:03 +0100

    Functions to run Wofost based on payloads passed from the API

---
+++ api/core/simulation.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 LEEP, University of Exeter (UK)
+# Mattia Mancini (m.c.mancini@exeter.ac.uk), July 2025
+# ====================================================
+"""
+simulation.py
+=============
+Module for core business logic of running individual WOFOST simulations
+"""
+
+import pandas as pd
+
+from api.utils.utils import apply_conversion
+from ukwofost.core.crop_manager import Crop, CropBuilder
+from ukwofost.core.defaults import defaults, soil_parameters, wofost_parameters
+from ukwofost.core.parcel import Parcel
+from ukwofost.core.simulation_manager import WofostSimulator
+
+
+def run_wofost_simulation(run):
+    """Run an instance of a crop in WOFOST."""
+    parcel = run.parcel_id
+    try:
+        parcel_obj = Parcel(parcel)
+        sim = WofostSimulator(
+            location=parcel_obj,
+            weather_provider="Mesoclim",
+            soil_provider="SoilGrids",
+        )
+
+        parameter_dict = run.model_dump()
+
+        nonstandard_parameters = {
+            key: value
+            for key, value in parameter_dict.items()
+            if key in wofost_parameters or key in soil_parameters
+        }
+
+        try:
+            crop_args = CropBuilder(pd.Series(parameter_dict))
+            crop_params = crop_args.crop_parameters
+            crop_to_run = Crop(
+                crop_args.calendar_year,
+                crop_args.crop,
+                **crop_params,
+            )
+
+        except Exception as e:
+            print(f"[WARN] Falling back to default crop parameters: {e}")
+            crop_management = defaults.get("management").get(crop_args.crop)
+            crop_to_run = Crop(
+                crop_args.calendar_year, crop_args.crop, **crop_management
+            )
+
+        crop_output = sim.run(
+            crop_or_rotation=crop_to_run,
+            output_flag="full",
+            **nonstandard_parameters,
+        ).reset_index(drop=False)
+
+        # crop_list = [list(d.keys())[0] for d in crop_rotation.crop_list]
+        # crop_indices = find_contiguous_sets(crop_output, "LAI")
+        # crop_column = [
+        #     crop_list[index - 1] if index > 0 else "fallow"
+        #     for index in crop_indices
+        # ]
+        crop_output["crop"] = crop_to_run.crop
+        crop_output["parcel_id"] = parcel
+        crop_output["year"] = crop_to_run.calendar_year
+        crop_output["variety"] = crop_to_run.variety
+        crop_output["yield"] = crop_output.apply(
+            apply_conversion, axis=1
+        )
+
+        return crop_output
+
+    except Exception as e:
+        print(f"Error for parcel {parcel}: {e}")
+        return pd.DataFrame()
