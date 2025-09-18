be08fb4b81e2f6944c153daa9d7ada1fb89c393b
Mattia Mancini <61020165+mcmancini@users.noreply.github.com>
2025-09-18 10:40:17 +0100

    Bug fixes when querying the SQL database in weather data providers (#111)
    * Bug fixes when querying the SQL database in weather data providers
    
    * minor formatting fixes

--- ukwofost/data_providers/weather_manager.py
+++ ukwofost/data_providers/weather_manager.py
@@ -145,7 +145,7 @@ class NetCDFWeatherDataProvider(WeatherDataProvider):
             self.osgrid_1km, epsg=4326
         )
 
-        self.elevation = get_dtm_values(osgrid_code, app_config)["elevation"]
+        self.elevation = get_dtm_values(osgrid_code)["elevation"]
         # pylint: enable=E1101
 
         # Retrieve Angstrom coefficients A and B
@@ -830,9 +830,7 @@ class Era5WeatherDataProvider(WeatherDataProvider):
             elevation = location.elevation
         elif isinstance(location, str):
             try:
-                elevation = round(
-                    get_dtm_values(location, app_config)["elevation"]
-                )
+                elevation = round(get_dtm_values(location)["elevation"])
             except ConnectionError:
                 elevation = 0
             return elevation
