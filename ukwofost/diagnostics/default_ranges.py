d0ce5db07679fb8c7246bfa9e92fcbc09e880a95
mcmancini <m.c.mancini@exeter.ac.uk>
2025-12-01 13:39:42 +0000

    Set default ranges for parameters that can be altered in the sampling procedures

---
+++ ukwofost/diagnostics/default_ranges.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 LEEP, University of Exeter (UK)
+# Mattia Mancini (m.c.mancini@exeter.ac.uk), November 2025
+# ========================================================
+"""
+Default parameter ranges for sampling and diagnostics
+=====================================================
+N.B. The default values are taken for the most part from
+the Wofost 8.1 documentation; values for PFFieldCapacity, 
+PFWiltingPoint and SurfaceConductivity are taken from 
+ChatGPT; these should be reviewed by an expert;
+There is a very good chance that the units of SurfaceConductivity
+are not cm/second as indicated in the Wofost 8.1 documentation
+at https://tinyurl.com/542zk4ca, but rather cm/day.
+-----------------------------------------------------------------
+"""
+parameter_defaults = {
+    "SSI": (0., [0., 100.], float),
+    "SSMAX": (0., [0., 100.], float),
+    "WAV": (None, [0., 6.], float),
+    "CRAIRC": (0.060, [0.04, 0.1], float),
+    "SMLIM": (0.6, [0., 1.], float),
+    "PFFieldCapacity": (2.0, [1.7, 3.5], float),
+    "PFWiltingPoint": (4.2, [2.70, 3.48], float),
+    "CO2": (None, [300., 1400.], float),
+    "SOPE": (1.47, [1.0, 2.0], float),
+    "NAVAILI": (80, [0., 250.], float),
+    "PAVAILI": (10, [0., 50.], float),
+    "KAVAILI": (20, [0., 250.], float),
+    "N_1": (0.0, [0.0, 625.0], float),
+    "N_2": (0.0, [0.0, 625.0], float),
+    "N_3": (0.0, [0.0, 625.0], float),
+    "N_4": (0.0, [0.0, 625.0], float),
+    "P_1": (0.0, [0.0, 625.0], float),
+    "P_2": (0.0, [0.0, 625.0], float),
+    "P_3": (0.0, [0.0, 625.0], float),
+    "P_4": (0.0, [0.0, 625.0], float),
+    "K_1": (0.0, [0.0, 625.0], float),
+    "K_2": (0.0, [0.0, 625.0], float),
+    "K_3": (0.0, [0.0, 625.0], float),
+    "K_4": (0.0, [0.0, 625.0], float),
+}
